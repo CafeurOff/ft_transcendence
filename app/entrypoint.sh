e18a075d5d3eb3d81821a58a5bbfcb7f9e1fc89b
Cafeur <cafeur@beggin.fr>
2024-03-22 18:58:43 +0100

    Rework of all containers, add managements of files

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,3 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py flush --no-input
+
+python manage.py makemigrations ft_transcendence
+python manage.py migrate
+
+exec "$@"
