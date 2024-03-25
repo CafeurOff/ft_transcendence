f3735edb8a86390db8ed8d8d2a66c087d5a8f5f5
Cafeur <cafeur@beggin.fr>
2024-03-25 02:11:01 +0100

    Add start of ELK logs, make somes comment and re-norms the project

--- app/ft_transcendence/settings.py
+++ app/ft_transcendence/settings.py
@@ -132,3 +132,24 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'logstash': {
+            'level': 'DEBUG',
+            'class': 'logstash.TCPLogstashHandler',
+            'host': 'logstash',
+            'port': 5000,
+            'version': 1,
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['logstash'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
