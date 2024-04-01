4617a2ee707076fbac9f56d928be4f4dd8201449
Cafeur <cafeur@beggin.fr>
2024-04-01 18:39:47 +0200

    Make the register page

--- app/singlepage/forms.py
+++ app/singlepage/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.validators import RegexValidator
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import MinLengthValidator
 
 class UsernamesForm(forms.Form):        
     usernames = forms.CharField(
@@ -30,3 +33,32 @@ class RememberForm(forms.Form):
         widget=forms.CheckboxInput(attrs={'style': 'width: 20px; height: 20px; margin-right: 10px; background-color: #262525;'}),  # Définir directement le style
         required=False
     )
+
+class SignupForm(UserCreationForm):
+    username = forms.CharField(
+        label='',
+        widget=forms.TextInput(attrs={'style': 'width: 300px; height: 40px; font-family: "Montserrat"; font-size: 16px; background-color: rgba(198, 182, 182, 0.1); padding: 10px; border: none; border-radius: 7px; color: white;', 'class': 'form-control placeholder-style', 'placeholder': 'Nom d’utilisateur'}),
+        required=True,
+        max_length=20,
+        min_length=1,
+        error_messages={'required': 'Veuillez entrer au moins un nom d’utilisateur'},
+        # validators=[
+        #     RegexValidator(
+        #         regex='^[a-zA-Z0-9]*$',
+        #         message='Votre nom d’utilisateur ne doit contenir que des lettres et des chiffres',
+        #     )
+        # ]
+    )
+    password1 = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(attrs={'style': 'width: 300px; height: 40px; font-family: "Montserrat"; font-size: 16px; background-color: rgba(198, 182, 182, 0.1); padding: 10px; border: none; border-radius: 7px; color: white;', 'class': 'form-control placeholder-style', 'placeholder': 'Mot de passe', 'color': 'white'}), # Ajout de la couleur du texte
+        error_messages={'required': 'Veuillez entrer un mot de passe'},
+        # validators=[
+        #     MinLengthValidator(8, message='Votre mot de passe doit contenir au moins 8 caractères'),
+        # ]
+    )
+    password2 = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(attrs={'style': 'width: 300px; height: 40px; font-family: "Montserrat"; font-size: 16px; background-color: rgba(198, 182, 182, 0.1); padding: 10px; border: none; border-radius: 7px; color: white;', 'class': 'form-control placeholder-style', 'placeholder': 'Confirmer le mot de passe', 'color': 'white'}), # Ajout de la couleur du texte
+    )
+
