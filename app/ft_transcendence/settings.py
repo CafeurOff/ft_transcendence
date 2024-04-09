a136b1c27a0c82f561fc5e12f438e9449b802399
Cafeur <cafeur@beggin.fr>
2024-04-09 16:16:17 +0200

    Change the language for error messages

--- app/ft_transcendence/settings.py
+++ app/ft_transcendence/settings.py
@@ -115,7 +115,7 @@ AUTH_USER_MODEL = 'singlepage.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
